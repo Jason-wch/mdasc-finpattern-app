92939801ee7652e3e08eb77f830a9df543f87791
w-Jazon <jason.wong.chy@gmail.com>
2022-11-01 22:28:19 +0800

    commit

---
+++ data_prep.py
@@ -0,0 +1,18 @@
+import pandas as pd
+import os
+
+stock_dir = './input/Stock Price/'
+stock_paths = [f for f in os.listdir(stock_dir) if f != '.DS_Store']
+
+stock_list = []
+dict_stock = {}
+
+for i in stock_paths:
+    dir = stock_dir + '/'+i
+    df = pd.read_csv(dir, index_col=0)
+    df['date'] = df.index
+    stock = i.split('_')[0]
+    dict_stock[stock] = df
+
+stocks = list(dict_stock.keys())
+stock_channels = dict_stock[stocks[0]]
